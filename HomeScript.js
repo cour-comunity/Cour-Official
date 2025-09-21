a40414c08028b1d94f0ec3b9af12f4f3a4504a44
Cour-Universal <courunivseral@gmail.com>
2025-09-21 16:59:25 -0700

    Create HomeScript.js

---
+++ HomeScript.js
@@ -0,0 +1,8 @@
+//should be the main script for index home, service script will be what gets imported into here
+//setup basic variables here
+
+
+
+window.onload = function() {
+  console.log('Stop Snooping Around in here theres nothing here yet');
+};
