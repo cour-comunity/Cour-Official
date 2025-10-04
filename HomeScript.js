80b6db02fe988b09f0de30d169e4eb78047622df
Cour-Universal <courunivseral@gmail.com>
2025-10-04 08:52:40 -0700

    Finished ui plants
    additions and patches ig

--- HomeScript.js
+++ HomeScript.js
@@ -1,7 +1,13 @@
 //should be the main script for index home, service script will be what gets imported into here
 //setup basic variables here
+//document.body.style.background = 'blue';
 ServiceManager = new ServicesManager();
 ServiceManager.From.HTMLServices.Import('StarterUIService');
+ServiceManager.From.HTMLServices.Import('UIImportService');
+ServiceManager.From.HTMLServices.Import('SideMenuService');
+ServiceManager.From.JSServices.Import('UserActions');
+
 
 window.onload = function() {
+  
 };
--- Service.js
+++ Service.js
@@ -64,7 +64,9 @@ class JSServicesManager {
       CacheForElement(Element) {
         
       }
+      
     }
+    return new Initilization(this.GlobalServiceManager);
   } //handles some functions with ui
   
   get AnimationManagerInitilization() {
@@ -73,7 +75,7 @@ class JSServicesManager {
         this.ServiceManager = Manager;
         this.AnimationQue = {
           /* Format
-          AnimationName: { Just the custom name can be auto assinged if not defined
+          name: { Just the custom name can be auto assinged if not defined
           
             Object //Required for a html element if animating an object
             
@@ -83,7 +85,7 @@ class JSServicesManager {
             Keyframe { //the keyframes name NEEDS to be the style were animating through style
               StartValue //starting INTIGER OR FLOAT for the value if not defined will auto become what it should be
               EndValue //ending INTIGER OR FLOAT for the value
-              Prefix //if your animating something life style.left include end prefix like PX or %
+              Suffix //if your animating something life style.left include end prefix like PX or %
               Curve //curve for this animation
               Regression //explanitory
               
@@ -103,50 +105,165 @@ class JSServicesManager {
         };
         
         this.Regressions = {
+          'Sigmoid': function Sigmoid(X, Data) {
+            if (!(Data.W1)) Data.W1 = 7.8;
+            if (!(Data.W2)) Data.W2 = 4;
+            if (!(Data.B1)) Data.B1 = -2.1;
+            return 1 / (1 + (Math.pow(W1, -1 * (w2 * X + B1))));
+          }
+        };
+        this.BiasCurve = {
+          
+        };
+        this.ActivationCurve = {
           
         };
       } //check this to find the format of animation que
-      MasterFrame(Frame) { //I will add heavy ammount of comments for ths for those snooping the code if they feel like they will use this service
-      
-        //loop through each name
+      MasterFrame(Frame) { 
         Array.from(Object.keys(this.AnimationQue)).forEach(AnimationName => {
-          if ('Object' in this.AnimationQue[AnimationName]) { //if were animating an object
-            Array.from(Object.keys(this.AnimationQue[AnimationName])).forEach(StyleToAnimate => {
-              
-              if (StyleToAnimate !== 'Object') { //make sure not to animate the object
-                let KeyframeData = this.AnimationQue[AnimationName][StyleToAnimate]; //this is to simplify the code
-                if (!KeyframeData.StartedAt) {
-                  KeyframeData.StartedAt = Frame;
+          let Animation = this.AnimationQue[AnimationName];
+          
+          if ('Element' in Animation) {
+            let ElementToAnimate = Animation.Element;
+            
+            Array.from(Object.keys(Animation)).forEach(KeyframeName => {
+              if (KeyframeName !== 'Element') {
+                let Keyframe = Animation[KeyframeName];
+                if (!(Keyframe.StartedAt)) {
+                  Keyframe.StartedAt = Frame;
                 }
-                if (!KeyframeData.StartValue) {
-                  KeyframeData.StartValue = parseInt(Element.style[StyleToAnimate], 10);
+                if (!(Keyframe.StartValue)) {
+                  Keyframe.StartValue = parseInt(ElementToAnimate.style[KeyframeName], 10);
                 }
-                let Completion = (Frame - KeyframeData.StartedAt) / KeyframeData.Duration;
-                if (Completion < 1) {
-                  //apply regression
-                  if ('Regression' in KeyframeData) {
-                    Completion = this.Regressions[KeyframeData.Regression[Regression]](KeyframeData.Regression.Data);
+                
+                let TrueCompletion = (Frame - Keyframe.StartedAt) / Keyframe.Duration;
+                let Completion = TrueCompletion; //true complotion is before regression
+                //apply regression
+                if ('Regression' in Keyframe) {
+                  Completion = this.Regressions[Keyframe.Regression.Type](Completion, Keyframe.Regression.Variables);
+                }
+                let NewValue = Completion * ((Keyframe.EndValue - Keyframe.StartValue) / Keyframe.Duration);
+                if ('ActivationCurve' in Keyframe) {
+                  NewValue = this.ActivationCurve[Keyframe.ActivationCurve.Type](NewValue, Keyframe.ActivationCurve.Data);
+                }
+                if ('BiasCurve' in Keyframe) {
+                  NewValue = NewValue + this.BiasCurve[Keyframe.BiasCurve.Type](TrueCompletion, Keyframe.BiasCurve.Data);
+                }
+                if (!(Keyframe.Suffix)) {
+                  Keyframe.Suffix = '';
+                }
+                ElementToAnimate.style[KeyframeName] = String(NewValue) + Keyframe.Suffix;
+                if (TrueCompletion >= 1) {
+                  ElementToAnimate.style[KeyframeName] = String(Keyframe.EndValue) + Keyframe.Suffix;
+                  //end animation
+                  if ('DoAfter' in Keyframe && typeof Keyframe.DoAfter === 'function') {
+                    Keyframe.DoAfter();
+                  } else if ('DoAfter' in Keyframe && typeof Keyframe.DoAfter === 'string') {
+                    this.ServiceManager.MessagingService.BroadcastToChannel(Keyframe.DoAfter);
                   }
-                  //calculate new value
-                  let NewValue = ((KeyframeData.EndValue - KeyframeData.StartValue) * Completion) + KeyframeData.StartValue;
-                } else {
-                  //do after here
+                  delete this[AnimationName][KeyframeName];
                 }
               }
             });
-          } else if ('WaitUntil' in this.AnimationQue[AnimationName] || 'Wait' in this.AnimationQue[AnimationName]) { //if were waiting 
-            
-          } else { //if nothing is in here to animate
+            if (Object.keys(Animation).length - 1 < 1) {
+              delete this.AnimationQue[AnimationName];
+            }
+          } else if ('Wait' in Animation) {
+            if (!(Animation.StartedAt)) {
+              Animation.StartedAt = Frame;
+            }
+            if (Animation.StartedAt + Animation.Duration <= Animation.StartedAt + Frame) {
+              if (typeof Animation.DoAfter === 'string') {
+                this.ServiceManager.MessagingService.BroadcastToChannel(Animation.DoAfter);
+              } else {
+                Animation.DoAfter();
+              }
+            }
+          } else if ('WaitUntill' in Animation) {
+            if (Animation.Duration()) {
+              if (typeof Animation.DoAfter === 'string') {
+                this.ServiceManager.MessagingService.BroadcastToChannel(Animation.DoAfter);
+              } else {
+                Animation.DoAfter();
+              }
+            }
+          } else {
             delete this.AnimationQue[AnimationName];
           }
         });
         
-        if (Object.keys(AnimationQue).length > 0) {
-          //this.Playing = requestAnimationFrame(this.MasterFrame.bind(this));
-        }
       } //this is the Central hub for managing animations and made easy.
     }
+    return new Initilization(this.GlobalServiceManager);
   } //work on this
+  
+  get UserActionsInitilization() {
+    class Initilization {
+      constructor(Manager) {
+        this.ServiceManager = Manager;
+        class Mouse {
+          constructor(Parent) {
+            this.ParentManager = Parent;
+            this.X = null;
+            this.Y = null;
+            this.LeftClick = false;
+            this.RightClick = false;
+            this.MiddleClick = false;
+            document.addEventListener('mousedown', this.MouseDown.bind(this));
+            document.addEventListener('mouseup', this.MouseUp.bind(this));
+            document.addEventListener('mousemove', this.MouseMove.bind(this));
+          }
+          
+          
+          MouseDown(Event) {
+            let Target = Event.target;
+            while (Target.dataset.passEventToParent) {
+              if (Target.dataset.passEventToParent.toLowerCase() === 'true') {
+                Target = Target.parentNode;
+              }
+            }
+            if (event.buttons === 1 && !(this.RightClick || this.MiddleClick) && String(Target.style.cursor)) { //Activate resize Mode
+              
+            }
+            /* event.buttons
+            1: Left mouse button.
+            2: Right mouse button.
+            4: Middle mouse button (wheel).
+            */
+          }
+          MouseUp(Event) {
+            let Target = Event.target;
+            while (Target.dataset.passEventToParent) {
+              if (Target.dataset.passEventToParent.toLowerCase() === 'true') {
+                Target = Target.parentNode;
+              }
+            }
+            if (!(this.RightClick || this.LeftClick || this.MiddleClick) && Target.tagName === 'RESIZABLE-WINDOW-V1') {
+              let TargetLeft = parseInt(Target.style.left);
+              let TargetTop = parseInt(Target.style.top);
+              let TargetRight = TargetLeft + parseInt(Target.style.width);
+              let TargetBottom = TargetTop + parseInt(Target.style.height);
+              
+            }//v1 will be for resizablewindows that do not have there own resize elements to save proformance and space
+            
+          }
+          MouseMove(Event) {
+            let Target = Event.target;
+            while (Target.dataset.passEventToParent) {
+              if (Target.dataset.passEventToParent.toLowerCase() === 'true') {
+                Target = Target.parentNode;
+              }
+            }
+            
+          }
+          
+        }
+        this.Mouse = new Mouse(this);
+        
+      }
+    }
+    return new Initilization(this.GlobalServiceManager);
+  }
   
   
   
@@ -171,6 +288,12 @@ class HTMLServicesManager {
   }
   
   get StarterUIServiceInitilization() {
+    class InitilizationClass {
+      constructor(Manager) {
+        this.ServiceManager = Manager;
+      }
+    }//a class the user can call in javascript to manage the tags
+    var InitilizedClass = new InitilizationClass(this.GlobalServiceManager);
     const CustomTags = {
       'starter-ui': class Initialization extends HTMLElement {
         constructor() {
@@ -187,11 +310,6 @@ class HTMLServicesManager {
             let ImportedUI = document.importNode(UI.content, true);
             let ImportedUIDestination = document.body;
             ImportedUIDestination.appendChild(ImportedUI);
-          }
-          if (typeof starterUIInitilization === 'function') {
-            starterUIInitilization();
-          } else if (typeof StarterUIInitilization === 'function') {
-            //add later
           }
           
           this.remove();
@@ -233,23 +351,96 @@ class HTMLServicesManager {
           this.remove();
         }
         
-      }
+      }, //runs everything else
+      'starter-ui-unpacked-setup-js': class StarterUIUnpackedSetupScript extends HTMLElement {
+        constructor() {
+          super();
+        }
+        connectedCallback() {
+          document.head.appendChild(this.firstChild);
+          if (StarterUIInitilizationScript && typeof StarterUIInitilizationScript === 'function') {
+            InitilizedClass.Service = new StarterUIInitilizationScript(InitilizedClass.ServiceManager);
+          }
+          this.remove();
+        }
+      } //runs setup javascript
     }; //make sure the tags are named propperly
     
     this.InitilizeElements(CustomTags); //initilize the tags
-    
-    class InitilizationClass {
-      constructor(Manager) {
-        this.Manager = Manager;
-      }
-    } //a class the user can call in javascript to manage the tags
+     
     
     
     //setup dependencys
     this.GlobalServiceManager.From.JSServices.Import('MessagingService');
     
-    return new InitilizationClass(this.GlobalServiceManager);
+    
+    return InitilizedClass;
   } //this is what you put templates for ui inside of to load the propper one later
+  
+  get UIImportServiceInitilization() {
+    class InitilizationClass {
+      constructor(Manager) {
+        this.ServiceManager = Manager;
+      }
+    }
+    var InitilizedClass = new InitilizationClass(this.GlobalServiceManager);
+    const CustomTags = {
+      'ui-import-service': class UiImportServiceInitilization extends HTMLElement {
+        constructor() {
+          super();
+          
+        }
+        connectedCallback() {
+          let ChildToImport = document.getElementById('ImportStorage' + this.id);
+          this.id = 'DeletingElementSoon';
+          
+          if (ChildToImport) {
+            let ImportedElement = document.importNode(ChildToImport.content, true);
+            
+            this.parentNode.appendChild(ImportedElement);
+            
+          }
+          this.remove();
+        }
+      }
+    };
+    this.InitilizeElements(CustomTags);
+    this.GlobalServiceManager.From.JSServices.Import('MessagingService');
+    return InitilizedClass;
+  }
+  
+  get SideMenuInitilization() {
+    class InitilizationClass {
+      constructor(Manager) {
+        this.ServiceManager = Manager;
+      }
+    }
+    
+    var InitilizedClass = new InitilizationClass(this.GlobalServiceManager);
+    this.GlobalServiceManager.From.JSService.Import('AnimationManager');
+    const CustomTags = {
+      'side-menu': class SDM extends HTMLElement {
+        constructor() {
+          super();
+        }
+        static get observedAttributes() {
+          return ['data-pinned'];
+        }
+        attributeChangedCallback(Name, OldValue, NewValue) {
+          if (Name === 'data-pinned') {
+            if (NewValue === 'True' && OldValue === 'False') {
+              
+            }
+          }
+        }
+      }
+    };
+    this.InitilizeElements(CustomTags);
+    return InitilizedClass;
+    
+  }
+  
+  
   Import(Service) {
     if (!(String(Service) in this.GlobalServiceManager)) {
       this.GlobalServiceManager[Service] = this[String(Service) + 'Initilization'];
@@ -278,10 +469,15 @@ class ServicesManager {
   Import(Service) {
     if (typeof Service === 'string' && globalThis[Service]) {
       this.From[Service] = globalThis[Service];
+    } else if (typeof Service === 'string' && GlobalExternalServices[Service]) {
+      this.From[Service] = new GlobalExternalServices[Service]();
     }
-
   }
-  
 }
 
 
+/* To Do
+add more services what do you think?
+
+
+*/
